9138e081241580e6b84422621456ef77a7d196be
Takanori Yoshino <yossii@mtf.biglobe.ne.jp>
2025-06-14 17:34:06 +0900

    月選択方法を変更(2)

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ from matplotlib.gridspec import GridSpec
 name = st.sidebar.selectbox('あなたの名前', ("孝則","由香"))
 year = st.sidebar.selectbox('測定した年', ("2024","2025","2026"))
 #month = st.sidebar.text_input('グラフ表示する月')
-options = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")
-month = st.sidebar.selectbox('グラフ表示する月', options)
+month = st.sidebar.selectbox('グラフ表示する月', 
+            ("1","2","3","4","5","6","7","8","9","10","11","12"))
 
 # 実行ボタン
 exec_btn =  st.sidebar.button("実行")
